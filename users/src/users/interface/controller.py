8cbf28bf7e0db8b1c2944166917fc57dbb69e917
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2022-05-06 05:15:20 -0500

    feat(users): use register and login in controller

--- users/src/users/interface/controller.py
+++ users/src/users/interface/controller.py
@@ -1,30 +1,37 @@
-from flask import Blueprint, Response, jsonify, request
-from resources.application.queries.get_resource_query import GetResourceQuery
-from shared.domain.exceptions.bad_request import BadRequestException
+from flask import Blueprint, request
 from shared.infraestructure.dependency_injection.app import App
 
-from ..application.commands.create_resource_command import \
-    CreateResourceCommand
+from users.application.commands.register_user_command import RegisterUserCommand
+from users.application.queries.user_login_query import UserLoginQuery
+from shared.domain.exceptions.unauthorized import UnauthorizedException
 
-resources_controller = Blueprint("resources_controller", __name__)
+users_controller = Blueprint("users_controller", __name__)
 command_bus = App().container.CommandBus()
 query_bus = App().container.QueryBus()
 
 
-@resources_controller.route("/resources/<id>", methods=["GET"])
-def get_resource(id):
-    id = request.view_args["id"]
-    try:
-        query = GetResourceQuery(id)
-        result = query_bus.execute(query)
-        return result.__dict__, 200
-    except BadRequestException as exception:
-        return {"error": str(exception)}, 404
-
-
-@ resources_controller.route("/resources", methods=["POST"])
-def create_resource():
+@ users_controller.route("/users", methods=["POST"])
+def register_user():
     body = request.get_json()
-    command = CreateResourceCommand(id=body["id"], name=body["name"])
+    command = RegisterUserCommand(
+        name=body["name"],
+        email=body["email"],
+        age=body["age"],
+        password=body["password"]
+    )
     command_bus.execute(command)
     return {}, 204
+
+
+@ users_controller.route("/users/login", methods=["POST"])
+def user_login():
+    body = request.get_json()
+    try:
+        query = UserLoginQuery(
+            email=body["email"],
+            password=body["password"]
+        )
+        result = query_bus.execute(query)
+        return result.__dict__, 200
+    except UnauthorizedException as exception:
+        return {"error": str(exception)}, 401
