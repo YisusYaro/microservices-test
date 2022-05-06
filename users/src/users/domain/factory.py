ffe12518c0ec672dd4cd20335469ca46d8bd237f
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2022-05-06 05:14:13 -0500

    feat(users): add factory

--- users/src/users/domain/factory.py
+++ users/src/users/domain/factory.py
@@ -1,11 +1,11 @@
-from resources.domain.resource import Resource
+from users.domain.user import User
 
 
 class Factory(object):
     @staticmethod
-    def create(id, name):
-        return Resource(id=id, name=name)
+    def create(id, name, email, age):
+        return User(id=id, name=name, email=email, age=age)
 
     @staticmethod
-    def reconstitute(id, name, status):
-        return Resource(id=id, name=name, status=status)
+    def reconstitute(id, name, email, age, password):
+        return User(id=id, name=name, email=email, age=age, password=password)
