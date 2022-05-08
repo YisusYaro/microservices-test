0d494459c5239fa2445ab2a38e5482207ce21956
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2022-05-08 02:22:08 -0500

    feat(characters):  add http service

--- server/src/characters/infraestructure/http_services/character_http_service.py
+++ server/src/characters/infraestructure/http_services/character_http_service.py
@@ -1,17 +1,55 @@
-import http.client
+import os
+from datetime import datetime
+from hashlib import md5
+
+import requests
+from characters.domain.factory import Factory
 
 
 class CharacterHttpService(object):
     def __init__(self):
-        self.__connection = http.client.HTTPSConnection(
-            "gateway.marvel.com/v1/public/characters")
-        self.__apikey = "e88d494087bf302528d097550fa4304b"
+        self.__public_key = os.environ.get("MARVEL_PUBLIC_KEY")
+        self.__private_key = os.environ.get("MARVEL_PRIVATE_KEY")
+        self.__path = "https://gateway.marvel.com/v1/public/characters"
+        self.__limit_pagination = 100
 
-    def list_characters_by_criteria(self, filters):
-        print("fetching")
+    def list_characters(self):
+        offset = 0
+        results = []
 
-        self.__connection.request(
-            "GET", '/v1/public/characters?apikey={apikey}'.format(apikey=self.__apikey))
+        while True:
+            page_results, count = self.__fetch_page(offset)
+            results = results = page_results
+            offset += self.__limit_pagination
+            if((count % self.__limit_pagination) != 0):
+                break
 
-        response = self.__connection.getresponse()
-        print("\n\n\n\n", response.read().decode())
+        return list(map(self.__result_to_character, results))
+
+    def __fetch_page(self, offset):
+        http_result = self.__fetch(offset)
+
+        json_result = http_result.json()
+
+        return json_result["data"]["results"], json_result["data"]["count"]
+
+    def __fetch(self, offset):
+        timestamp = self.__get_timestamp()
+        hash = self.__get_hash(timestamp)
+        return requests.get(
+            '{path}?limit={limit}&offset={offset}&ts={ts}&apikey={apikey}&hash={hash}'.format(path=self.__path, limit=self.__limit_pagination, offset=offset, ts=timestamp, apikey=self.__public_key, hash=hash))
+
+    def __result_to_character(self, result):
+        image = '{path}.{extension}'.format(
+            path=result["thumbnail"]["path"], extension=result["thumbnail"]["extension"])
+
+        appearances = result["stories"]["items"] + \
+            result["events"]["items"] + result["series"]["items"]
+
+        return Factory.reconstitute(id=result["id"], name=result["name"], image=image, appearances=appearances)
+
+    def __get_hash(self, timestamp):
+        return md5((timestamp+self.__private_key+self.__public_key).encode('utf-8')).hexdigest()
+
+    def __get_timestamp(self):
+        return str(datetime.timestamp(datetime.now()))
