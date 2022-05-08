0a8b3cffadcc64047457bbe44d1129a00c600de9
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2022-05-07 22:56:54 -0500

    feat(characters-and-comics): format query filters

--- server/src/shared/interface/controller.py
+++ server/src/shared/interface/controller.py
@@ -13,12 +13,15 @@ query_bus = App().container.QueryBus()
 @shared_controller.route("/searchComics/", methods=["GET"])
 @handle_http_error()
 def list_characters_by_criteria():
-    filters = request.args.getlist('filters')
-    for filter in filters:
-        print(filter)
-    # query = ListCharactersByCriteriaQuery(
-    #     filters=filters,
-    # )
-    # result = query_bus.execute(query)
-    # return result.__dict__, 200
-    return {}, 200
+    raw_filters = request.args.getlist('filters')
+
+    formated_filters = map(format_filter, raw_filters)
+    
+    query = ListCharactersByCriteriaQuery(
+        filters=formated_filters,
+    )
+    result = query_bus.execute(query)
+    return result.__dict__, 200
+
+def format_filter(filter):
+    return json.loads(filter)
